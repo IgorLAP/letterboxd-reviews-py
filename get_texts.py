629e65060527ee352bb8c42fe1b1a6384a13f20b
IgorLAP <ilap1036@gmail.com>
2024-09-18 11:48:28 -0300

    fix: get one review
    Signed-off-by: IgorLAP <ilap1036@gmail.com>

--- get_texts.py
+++ get_texts.py
@@ -24,7 +24,7 @@ def get_texts(last_page: int, user_name: str, initial_page=1, query=''):
         for index in range(len(headliners)):
             if index % 2 == 0:
                 if query != '':
-                    movie_names.append(headliners[index].text)
+                    movie_names.append(headliners[index].text.lower())
                 else:
                     links_and_names.append({
                         'link': f'{BASE_URL}{headliners[index].get("href")}',
@@ -32,26 +32,32 @@ def get_texts(last_page: int, user_name: str, initial_page=1, query=''):
                     })
             else:
                 years.append(headliners[index].text)
-        if query != '' and movie_names.__contains__(query) is False:
+        if query and query.lower() not in movie_names:
             if counter + 1 > loop_count and len(texts) == 0:
                 print('Review was not found')
             continue
-        elif query != '' and movie_names.__contains__(query) is True:
-            movie_found = reviews_page.find('a', string=query)
-            url = f'{BASE_URL}{movie_found.get("href")}'
-            review_page = BeautifulSoup(
-                requests.get(url).content, 'html.parser'
-            )
-            paragraphs = review_page.select('.review div div p')
-            review = []
-            for item in paragraphs:
-                review.append(item.text)
-            texts.append({
-                'movie': movie_found.text,
-                'year': review_page.find('small', attrs={'class': 'metadata'}).text,
-                'review': review
-            })
-            break
+        elif query and query.lower() in movie_names:
+            movie_found = None
+            for a_tag in reviews_page.find_all('a'):
+                if query.lower() in a_tag.text.lower():
+                    movie_found = a_tag
+                    break
+
+            if movie_found:
+                url = f'{BASE_URL}{movie_found.get("href")}'
+                review_page = BeautifulSoup(
+                    requests.get(url).content, 'html.parser'
+                )
+                paragraphs = review_page.select('.review div div p')
+                review = []
+                for item in paragraphs:
+                    review.append(item.text)
+                texts.append({
+                    'movie': movie_found.text,
+                    'year': review_page.select('small.metadata a')[0].text,
+                    'review': review
+                })
+                break
         else:
             counter = 0
             for movie in links_and_names:
